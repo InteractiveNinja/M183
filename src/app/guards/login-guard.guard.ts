72143d13794908824069630ccc1a7a5f79019e17
InteractiveNinja <gabrielnadolny2@gmail.com>
2022-06-14 10:30:43 +0200

    route guard navigate back to login site when not permitted

--- src/app/guards/login-guard.guard.ts
+++ src/app/guards/login-guard.guard.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
-  CanActivate,
+  CanActivate, Router,
   RouterStateSnapshot,
   UrlTree,
 } from '@angular/router';
-import { Observable } from 'rxjs';
+import {map, Observable} from 'rxjs';
 import { LoginService } from '../service/login/login.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoginGuardGuard implements CanActivate {
-  constructor(private readonly service: LoginService) {}
+  constructor(private readonly service: LoginService, private readonly router: Router) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
@@ -22,6 +22,11 @@ export class LoginGuardGuard implements CanActivate {
     | Promise<boolean | UrlTree>
     | boolean
     | UrlTree {
-    return this.service.isLoggedIn();
+    return this.service.isLoggedIn().pipe(map(e => {
+      if(!e) {
+        this.router.navigateByUrl("/");
+      }
+      return e;
+    }))
   }
 }
