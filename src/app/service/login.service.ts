2b2d224a991037986e3d0b335c7f55b18abcb5ce
InteractiveNinja <gabrielnadolny2@gmail.com>
2022-06-14 10:02:49 +0200

    sets user in login service when user logs in

--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -3,24 +3,27 @@ import {BehaviorSubject, catchError, map, Observable, of, take} from 'rxjs';
 import {LoginData} from './login.interface';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../environments/environment";
+import {User} from "./user.model";
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoginService {
   private isLoggedIn$ = new BehaviorSubject<boolean>(false);
-
+  private user$ = new BehaviorSubject<User | undefined>(undefined);
   constructor(private readonly http: HttpClient) {
   }
 
   public login(credentials: LoginData): Observable<boolean> {
-    const login$ = this.http.post(`${environment.api}/login`, {
+    const login$ = this.http.post<User>(`${environment.api}/login`, {
       username: credentials.username,
       password: credentials.password
     }, {observe: 'response'})
       .pipe(
         take(1),
-        map(() => {
+        map((e) => {
+          if(e.body) this.user$.next(e.body);
+          console.log("user set");
           return true;
         }),
         catchError(() => {
@@ -29,6 +32,10 @@ export class LoginService {
       );
     login$.subscribe(value => this.setStatus(value))
     return login$;
+  }
+
+  public getUser(): Observable<User | undefined> {
+    return this.user$;
   }
 
   public isLoggedIn(): Observable<boolean> {
---
+++ src/app/service/user.model.ts
@@ -0,0 +1,11 @@
+export interface User{
+  id: number;
+  username: string;
+  password: string;
+  firstname: string;
+  lastname: string;
+  gender: string;
+  address: string;
+  city: string;
+  job: string;
+}
