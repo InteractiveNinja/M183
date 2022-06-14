2ba526de2e8f7dc4fe75a6fcde02008c4b6332ad
InteractiveNinja <gabrielnadolny2@gmail.com>
2022-06-14 09:54:12 +0200

    navigate to bills page when login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import {LoginService} from '../../service/login.service';
 import {FormBuilder, Validators} from "@angular/forms";
+import {take} from "rxjs";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -16,10 +18,17 @@ export class LoginComponent {
     [this.passwordForm]: ['', Validators.required]
   })
 
-  constructor(private readonly service: LoginService, private readonly fb: FormBuilder) {
+  constructor(private readonly service: LoginService, private readonly fb: FormBuilder, private router: Router) {
   }
 
   public login(): void {
-    this.service.login(this.loginFormGroup.value);
+    this.service.login(this.loginFormGroup.value).pipe(take(1)).subscribe(success => {
+      if(success) {
+        alert("Du bist jetzt eingeloggt!")
+        this.router.navigateByUrl("bills");
+        return;
+      }
+      alert("Password oder Nutzername war falsch!")
+    })
   }
 }
