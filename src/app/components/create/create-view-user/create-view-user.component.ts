9fd6e31c9b76a8de13f8e3c3dc6d107935681fba
InteractiveNinja <gabrielnadolny2@gmail.com>
2022-12-16 14:49:03 +0100

    validating user creation

--- src/app/components/create/create-view-user/create-view-user.component.ts
+++ src/app/components/create/create-view-user/create-view-user.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { UsersService } from '../../../service/users/users.service';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { take } from 'rxjs';
+import { UsersService } from '../../../service/users/users.service';
 
 @Component({
   selector: 'app-create-view-user',
@@ -10,25 +10,74 @@ import { take } from 'rxjs';
   styleUrls: ['./create-view-user.component.scss'],
 })
 export class CreateViewUserComponent {
-  public usernameForm = 'username';
-  public passwordForm = 'password';
-  public firstnameForm = 'firstname';
-  public lastnameForm = 'lastname';
-  public genderForm = 'gender';
-  public addressForm = 'address';
-  public cityForm = 'city';
-  public jobForm = 'job';
-  public adminForm = 'admin';
+  public usernameFormName = 'username';
+  public passwordFormName = 'password';
+  public firstnameFormName = 'firstname';
+  public lastnameFormName = 'lastname';
+  public genderFormName = 'gender';
+  public addressFormName = 'address';
+  public cityFormName = 'city';
+  public jobFormName = 'job';
+  public adminFormName = 'admin';
+
+  public usernameForm = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^([a-z0-9]+)$/i),
+    Validators.minLength(6),
+    Validators.maxLength(16),
+  ]);
+  public passwordForm = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^([a-z0-9!"#$%&'()*+,-./\\:;<=>?@\[\]^_`{|}~]+)$/i),
+    Validators.minLength(8),
+    Validators.maxLength(65),
+  ]);
+  public firstnameForm = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^([a-z]+)$/i),
+    Validators.minLength(1),
+    Validators.maxLength(25),
+  ]);
+  public lastnameForm = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^([a-z]+)$/i),
+    Validators.minLength(1),
+    Validators.maxLength(25),
+  ]);
+  public genderForm = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^([mfo])$/i),
+  ]);
+  public addressForm = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(\w+)$/i),
+    Validators.minLength(1),
+    Validators.maxLength(64),
+  ]);
+  public cityForm = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^([a-z]+)$/i),
+    Validators.minLength(1),
+    Validators.maxLength(16),
+  ]);
+  public jobForm = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^([a-z]+)$/i),
+    Validators.minLength(1),
+    Validators.maxLength(32),
+  ]);
+  public adminForm = new FormControl('');
+
   public userForm = this.fb.group({
-    [this.usernameForm]: ['', Validators.required],
-    [this.passwordForm]: ['', Validators.required],
-    [this.firstnameForm]: ['', Validators.required],
-    [this.lastnameForm]: ['', Validators.required],
-    [this.genderForm]: ['', Validators.required],
-    [this.addressForm]: ['', Validators.required],
-    [this.cityForm]: ['', Validators.required],
-    [this.jobForm]: ['', Validators.required],
-    [this.adminForm]: [''],
+    [this.usernameFormName]: this.usernameForm,
+    [this.passwordFormName]: this.passwordForm,
+    [this.firstnameFormName]: this.firstnameForm,
+    [this.lastnameFormName]: this.lastnameForm,
+    [this.genderFormName]: this.genderForm,
+    [this.addressFormName]: this.addressForm,
+    [this.cityFormName]: this.cityForm,
+    [this.jobFormName]: this.jobForm,
+    [this.adminFormName]: this.adminForm,
   });
 
   constructor(
