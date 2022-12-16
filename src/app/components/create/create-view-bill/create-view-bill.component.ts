af3e647a9af82d68e235c47c196aeea8c86d232c
InteractiveNinja <gabrielnadolny2@gmail.com>
2022-12-16 14:21:34 +0100

    validate bill creation

--- src/app/components/create/create-view-bill/create-view-bill.component.ts
+++ src/app/components/create/create-view-bill/create-view-bill.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-import { UsersService } from '../../../service/users/users.service';
-import { BillService } from '../../../service/bills/bill.service';
-import { Observable, take } from 'rxjs';
-import { User } from '../../../service/login/user.model';
-import { FormBuilder, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
+import { Observable, take } from 'rxjs';
+import { BillService } from '../../../service/bills/bill.service';
+import { User } from '../../../service/login/user.model';
+import { UsersService } from '../../../service/users/users.service';
 
 @Component({
   selector: 'app-create-view-bill',
@@ -14,13 +21,24 @@ import { Router } from '@angular/router';
 export class CreateViewBillComponent {
   public users$: Observable<User[]>;
 
-  public amountForm = 'amount';
-  public deadlineForm = 'deadline';
-  public userForm = 'user';
+  public amountFormName = 'amount';
+  public deadlineFormName = 'deadline';
+  public userFormName = 'user';
+
+  public amountForm = new FormControl(null, [
+    Validators.required,
+    Validators.min(1),
+  ]);
+  public deadlineForm = new FormControl(null, [
+    Validators.required,
+    this.futureValidator(),
+  ]);
+  public userForm = new FormControl('', [Validators.required]);
+
   public billForm = this.fb.group({
-    [this.amountForm]: ['', Validators.required],
-    [this.deadlineForm]: ['', Validators.required],
-    [this.userForm]: ['', Validators.required],
+    [this.amountFormName]: this.amountForm,
+    [this.deadlineFormName]: this.deadlineForm,
+    [this.userFormName]: this.userForm,
   });
 
   constructor(
@@ -30,6 +48,15 @@ export class CreateViewBillComponent {
     private readonly router: Router
   ) {
     this.users$ = usersService.getAllUsers();
+  }
+
+  private futureValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const date = new Date(control.value);
+      const today = new Date();
+      let biggerThenToday = date > today;
+      return biggerThenToday ? null : { futureValidator: { value: date } };
+    };
   }
 
   createBill() {
