9f205e6c4d0d3558e38a693dbe84ef5db97916b7
andygoeroeg <65227793+andygoeroeg@users.noreply.github.com>
2022-12-19 10:24:28 +0100

    Logging base Implementation (#18)
    * Added npm winston
    
    * ignore created logs
    
    * wrap winston in logger class
    
    * default is info -> removed, cleanup
    
    Co-authored-by: andygoeroeg <andygoeroeg@gmail.com>

--- server/util/logger.ts
+++ server/util/logger.ts
@@ -1,19 +1,32 @@
+import * as winston from "winston";
+
 enum LOGLEVELS {
-  WARN,
-  INFO,
-  ERROR,
+  WARN = "warn",
+  INFO = "info",
+  ERROR = "error",
 }
+
 export class Logger {
-  private constructor() {}
+
+  private static logger = winston.createLogger({
+    format: winston.format.json(),
+    transports: [
+      new winston.transports.File({ filename: "logs/error.log", level: LOGLEVELS.ERROR }),
+      new winston.transports.File({ filename: "logs/combined.log" }),
+      new winston.transports.Console({
+        format: winston.format.simple()
+      })
+    ]
+  });
 
   public static log(msg: string) {
-    this.logging(LOGLEVELS.INFO, msg);
-  }
-  public static warn(msg: string) {
-    this.logging(LOGLEVELS.WARN, msg);
+    this.logger.log(LOGLEVELS.INFO,msg);
   }
 
-  private static logging(logLevel: LOGLEVELS, msg: string) {
-    console.log(`[${LOGLEVELS[logLevel]}] ${msg}`);
+  public static warn(msg: string) {
+    this.logger.log(LOGLEVELS.WARN, msg);
+  }
+  public static error(msg: string) {
+    this.logger.log(LOGLEVELS.ERROR, msg);
   }
 }
