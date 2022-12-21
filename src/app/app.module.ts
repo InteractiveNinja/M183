6d48d923bc95413be298bb1cfa50a371dc80130e
Gabriel Nadolny <39236516+InteractiveNinja@users.noreply.github.com>
2022-12-21 12:19:15 +0100

    Frontend requirements changes (#28)
    * adds bootstrap-icons
    
    * combines bootstrap-icons into angular project
    
    * adds footer component to add username to the UI
    
    * adds admin bills view and adds service methode for deleting bills
    
    * sets route permission on admin routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,27 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { BillsViewComponent } from './components/bills/bills-view.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
-import { LoginGuardGuard } from './guards/login/login-guard.guard';
-import { LoginService } from './service/login/login.service';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { BillDetailAdminComponent } from './components/bills-admin/bill-detail/bill-detail-admin.component';
+import { BillsAdminViewComponent } from './components/bills-admin/bills-admin-view.component';
 import { BillDetailComponent } from './components/bills/bill-detail/bill-detail.component';
-import { UsersViewComponent } from './components/users/users-view.component';
-import { UserDetailComponent } from './components/users/user-detail/user-detail.component';
-import { AdminGuardGuard } from './guards/admin/admin-guard.guard';
-import { BillService } from './service/bills/bill.service';
-import { UserEditDetailComponent } from './components/user-edit-view/user-edit-detail/user-edit-detail.component';
-import { UserEditViewComponent } from './components/user-edit-view/user-edit/user-edit-view.component';
-import { CreateViewComponent } from './components/create/create-view/create-view.component';
+import { BillsViewComponent } from './components/bills/bills-view.component';
 import { CreateViewBillComponent } from './components/create/create-view-bill/create-view-bill.component';
 import { CreateViewUserComponent } from './components/create/create-view-user/create-view-user.component';
+import { CreateViewComponent } from './components/create/create-view/create-view.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { UserEditDetailComponent } from './components/user-edit-view/user-edit-detail/user-edit-detail.component';
+import { UserEditViewComponent } from './components/user-edit-view/user-edit/user-edit-view.component';
+import { UserDetailComponent } from './components/users/user-detail/user-detail.component';
+import { UsersViewComponent } from './components/users/users-view.component';
+import { AdminGuardGuard } from './guards/admin/admin-guard.guard';
+import { LoginGuardGuard } from './guards/login/login-guard.guard';
+import { BillService } from './service/bills/bill.service';
+import { LoginService } from './service/login/login.service';
 
 @NgModule({
   declarations: [
@@ -36,6 +38,9 @@ import { CreateViewUserComponent } from './components/create/create-view-user/cr
     CreateViewComponent,
     CreateViewBillComponent,
     CreateViewUserComponent,
+    FooterComponent,
+    BillsAdminViewComponent,
+    BillDetailAdminComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
