fbde799a086a6d9274590ca9f0cf09ec711ce4d6
InteractiveNinja <gabrielnadolny2@gmail.com>
2022-12-23 15:12:19 +0100

    reformats server.ts

--- server.ts
+++ server.ts
@@ -1,14 +1,14 @@
 import 'zone.js/dist/zone-node';
 
-import { ngExpressEngine } from '@nguniversal/express-engine';
+import {ngExpressEngine} from '@nguniversal/express-engine';
 import * as express from 'express';
-import { join } from 'path';
+import {join} from 'path';
 
-import { AppServerModule } from './src/main.server';
-import { APP_BASE_HREF } from '@angular/common';
-import { existsSync } from 'fs';
-import { apiRoutes } from './server/rest/routes';
-import {Logger} from "./server/util/logger";
+import {AppServerModule} from './src/main.server';
+import {APP_BASE_HREF} from '@angular/common';
+import {existsSync} from 'fs';
+import {apiRoutes} from './server/rest/routes';
+import {Logger} from './server/util/logger';
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
